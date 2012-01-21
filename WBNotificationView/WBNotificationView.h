4f500f412cccb4edee4b5d2fb48fc1dd3f6c1f0b
Rod Wilhelmy <rwilhelmy@gmail.com>
2012-01-21 00:48:46 -0600

    Added some documentation

--- WBNotificationView/WBNotificationView.h
+++ WBNotificationView/WBNotificationView.h
@@ -31,15 +31,30 @@ typedef enum {
 @property (nonatomic, copy) NSString *message;
 @property (nonatomic, weak) id <WBNotificationViewDelegate> delegate;
 
-// Sets message and notification type
-// TODO: 
-// - Support landscape mode
+/**
+ * WBNotificationView setup
+ * TODO:
+ * - Support landscape mode
+ * @param message: notification's text 
+ * @param type: a constant that determines notification's color and default message
+ * @return id: a WBNotificationView instance
+ * @discussion the view is initially positioned just above (0, 0)
+ */
 - (id)initWithMessage:(NSString *)message ofType:(WBNotificationViewType)type;
-// Moves the frame on top of the screen
+/**
+ * Slides the view on screen
+ * @discussion will use Core Animation to slide view on screen
+ */
 - (void)slideIn;
-// Moves the frame just above the screen
+/**
+ * Slides the view off screen
+ * @discussion will use Core Animation to slide view off screen (just above screen's top border)
+ */
 - (void)slideOut;
-// Performs a slide-in/slide-out delayed transition
+/**
+ * Chains a slide in transition to a slide out transition
+ * @param seconds: a delay interval between transitions
+ */
 - (void)slideInDisappearingIn:(NSTimeInterval)seconds;
 
 @end
